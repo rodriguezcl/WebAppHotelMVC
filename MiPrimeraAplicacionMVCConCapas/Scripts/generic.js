dcbfa6d9280c70142982aefa6864b3eec85f265d
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-09-20 15:25:41 -0300

    50.Configurar Editar y Eliminar opciones FINALIZADO OK

--- MiPrimeraAplicacionMVCConCapas/Scripts/generic.js
+++ MiPrimeraAplicacionMVCConCapas/Scripts/generic.js
@@ -153,7 +153,7 @@ function pintar(objConfiguracion, objBusqueda,objFormulario) {
                 contenido +=  ` </div>`
             }
             contenido += "<div id='divContenedor'>";
-            contenido += generarTabla(objConfiguracion, res);
+            contenido += generarTabla(objConfiguracion, res, objFormulario);
             contenido += "</div>";
             document.getElementById(objConfiguracion.id).innerHTML = contenido;
 
@@ -172,7 +172,10 @@ function LimpiarDatos(idFormulario,excepciones=[]) {
 }
 
 
-function generarTabla(objConfiguracion, res) {
+function generarTabla(objConfiguracion, res, objFormulario) {
+
+    //objFormulario.formularioGenerico = true
+
     var contenido = "";
     contenido += "<table class='table'>";
     contenido += "<tr>";
@@ -200,7 +203,7 @@ function generarTabla(objConfiguracion, res) {
             contenido += "<td>";
             if (objConfiguracion.editar == true) {
                 contenido += ` <i class="btn btn-primary" 
-               onclick='${objConfiguracion.callbackEditar}(${fila[objConfiguracion.propiedadId]})' >
+               onclick='${(objFormulario != undefined && objFormulario.formulariogenerico != undefined && objFormulario.formulariogenerico==true) ? "EditarGenerico" : objConfiguracion.callbackEditar}(${fila[objConfiguracion.propiedadId]})' >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyedropper" viewBox="0 0 16 16">
                     <path d="M13.354.646a1.207 1.207 0 0 0-1.708 0L8.5 3.793l-.646-.647a.5.5 0 1 0-.708.708L8.293 5l-7.147 7.146A.5.5 0 0 0 1 12.5v1.793l-.854.854a.5.5 0 1 0 .708.707L1.707 15H3.5a.5.5 0 0 0 .354-.146L11 7.707l1.146 1.147a.5.5 0 0 0 .708-.708l-.647-.646 3.147-3.146a1.207 1.207 0 0 0 0-1.708l-2-2zM2 12.707l7-7L10.293 7l-7 7H2v-1.293z" />
                 </svg></i>`
@@ -208,7 +211,7 @@ function generarTabla(objConfiguracion, res) {
 
             if (objConfiguracion.eliminar == true) {
                 contenido += `<i class="btn btn-danger" 
-                onclick='${objConfiguracion.callbackEliminar}(${fila[objConfiguracion.propiedadId]})'  ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                onclick='${(objFormulario != undefined && objFormulario.formulariogenerico != undefined && objFormulario.formulariogenerico == true) ? "EliminarGenerico" : objConfiguracion.callbackEliminar}(${fila[objConfiguracion.propiedadId]})'  ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                        </svg></i>`
             }
@@ -397,3 +400,12 @@ function GuardarGenerico(idformulario, urlguardar) {
         }
     })
 }
+
+function EditarGenerico(id) {
+
+}
+
+function EliminarGenerico(id) {
+
+}
+
