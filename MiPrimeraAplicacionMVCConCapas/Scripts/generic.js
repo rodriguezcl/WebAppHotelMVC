d2467992cfba610ace3ec986a3a367a01c12243a
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-09-20 19:36:27 -0300

    51. Recuperar método FINALIZADO OK

--- MiPrimeraAplicacionMVCConCapas/Scripts/generic.js
+++ MiPrimeraAplicacionMVCConCapas/Scripts/generic.js
@@ -200,10 +200,11 @@ function generarTabla(objConfiguracion, res, objFormulario) {
         ////contenido += "<td>" + fila.nombre + "</td>";
         ////contenido += "<td>" + fila.descripcion + "</td>";
         if (objConfiguracion.editar == true || objConfiguracion.eliminar == true) {
+            
             contenido += "<td>";
             if (objConfiguracion.editar == true) {
                 contenido += ` <i class="btn btn-primary" 
-               onclick='${(objFormulario != undefined && objFormulario.formulariogenerico != undefined && objFormulario.formulariogenerico==true) ? "EditarGenerico" : objConfiguracion.callbackEditar}(${fila[objConfiguracion.propiedadId]})' >
+               onclick='${(objFormulario != undefined && objFormulario.formulariogenerico != undefined && objFormulario.formulariogenerico == true) ? "EditarGenerico" : objConfiguracion.callbackEditar}(${fila[objConfiguracion.propiedadId]},"${objFormulario==undefined? "": objFormulario.id}")' >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyedropper" viewBox="0 0 16 16">
                     <path d="M13.354.646a1.207 1.207 0 0 0-1.708 0L8.5 3.793l-.646-.647a.5.5 0 1 0-.708.708L8.293 5l-7.147 7.146A.5.5 0 0 0 1 12.5v1.793l-.854.854a.5.5 0 1 0 .708.707L1.707 15H3.5a.5.5 0 0 0 .354-.146L11 7.707l1.146 1.147a.5.5 0 0 0 .708-.708l-.647-.646 3.147-3.146a1.207 1.207 0 0 0 0-1.708l-2-2zM2 12.707l7-7L10.293 7l-7 7H2v-1.293z" />
                 </svg></i>`
@@ -401,11 +402,24 @@ function GuardarGenerico(idformulario, urlguardar) {
     })
 }
 
-function EditarGenerico(id) {
-
+function EditarGenerico(id, idFormulario) {
+    //var idFormulario = "frmCama";
+    //var idformulario = objConfiguracionGlobal;
+    alert(idFormulario);
+    var url = objConfiguracionGlobal.urlRecuperar;
+    var nombreparametro = objConfiguracionGlobal.parametroRecuperar;
+    recuperarGenerico(`${url}/?${nombreparametro}=` + id, idFormulario);
 }
 
-function EliminarGenerico(id) {
+function EliminarGenerico(id,idformulario,nombreparametro) {
+    Confirmacion("Desea eliminar el tipo habitacion?", "Confirmar eliminaciòn", function (res) {
 
+        fetchGetText("TipoHabitacion/eliminarTipoHabitacion/?id=" + id, function (rpta) {
+            if (rpta == "1") {
+                Correcto("Se elimino correctamente");
+                listarTipoHabitacion();
+            }
+        })
+    })
 }
 
