adc23d393a4e046df4d4880614f6eecd1a4c2c39
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-09-23 15:26:36 -0300

    Realizar Procedure con 2 tablas via inner join FINALIZADO OK

---
+++ Capa Entidad/PersonaCLS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class PersonaCLS
+    {
+        public int iidpersona { get; set; }
+        public string nombreCompleto { get; set; }
+        public string nombreSexo { get; set; }
+        public string nombreTipoUsuario { get; set; }
+    }
+}
