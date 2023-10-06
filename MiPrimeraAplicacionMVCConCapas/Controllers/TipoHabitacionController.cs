9f6d546311513c96ced21cd3f669c40dd7099039
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-10-06 15:51:14 -0300

    94.Guardar un Radio Button dinámicamente FINALIZADO OK

--- MiPrimeraAplicacionMVCConCapas/Controllers/TipoHabitacionController.cs
+++ MiPrimeraAplicacionMVCConCapas/Controllers/TipoHabitacionController.cs
@@ -16,32 +16,6 @@ namespace MiPrimeraAplicacionMVCConCapas.Controllers
             return View();
         }
 
-        public ActionResult Inicio()
-        {
-            return View();
-        }
-
-        public ActionResult VistaPruebaInicio()
-        {
-            return View();
-        }
-
-        public string cadena()
-        {
-            return "123";
-        }
-
-        public int numero()
-        {
-            return 5;
-        }
-
-        //public List<TipoHabitacionCLS> lista()
-        //{
-        //    TipoHabitacionBL obj = new TipoHabitacionBL();
-        //    return obj.listarTipoHabitacion();
-        //}
-        // [{"id":1,"nombre":"Simple","descripcion":"Solo para uno"},{"id":2,"nombre":"Doble","descripcion":"Hecho para casados"}]
         public JsonResult lista()
         {
             TipoHabitacionBL obj = new TipoHabitacionBL();
@@ -55,12 +29,10 @@ namespace MiPrimeraAplicacionMVCConCapas.Controllers
                 JsonRequestBehavior.AllowGet);
         }
 
-
         public int guardarDatos(TipoHabitacionCLS oTipoHabitacionCLS)
         {
             TipoHabitacionBL obj = new TipoHabitacionBL();
             return obj.guardarTipoHabitacion(oTipoHabitacionCLS);
-
         }
 
         public JsonResult recuperarTipoHabitacion(int id)
