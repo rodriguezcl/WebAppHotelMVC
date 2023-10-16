b2b7562fb3075b5da4640812a87d8c65bf6e8d0f
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-10-16 18:00:57 -0300

    120.Uso del drd.NextResult en ADO.NET FINALIZADO OK

---
+++ Capa Entidad/HabitacionCLS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class HabitacionCLS
+    {
+        public int iidhabitacion { get; set; }
+        public string nombre{ get; set; }
+        public decimal precionoche { get; set; }
+        public string numeropersonas { get; set; }
+        public string textotienepiscina { get; set; }
+        public string textotienewifi { get; set; }
+        public string textotienevistaalmar { get; set; }
+    }
+}
